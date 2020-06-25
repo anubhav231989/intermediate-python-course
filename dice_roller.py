b0d80975fa0eda5ae06a81fc03d3acd3f098f2f1
Anubhav Sidhu <anubhavsidhu@Anubhavs-MacBook-Pro.local>
2020-06-25 13:25:36 +0530

    Adds user inputs to number of dice and dice size..

--- dice_roller.py
+++ dice_roller.py
@@ -1,10 +1,15 @@
 import random
 
 def main():
-  dice_rolls = 2
+  try:
+    dice_rolls = int(input("How many dice you want to roll?"))
+    dice_size = int(input("How many sides of the dice?"))
+  except:
+    dice_rolls = 2
+
   dice_sum = 0
   for i in range(0,dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum += roll
     ## f-sting functionality.
     print(f'You rolled a {roll}')
